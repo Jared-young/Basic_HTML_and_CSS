967adde02e20d43aa9854a963e87e6245ddf68fd
Jared-young <54957290+Jared-young@users.noreply.github.com>
2023-10-11 14:48:15 -0400

    VS19

---
+++ VS19BasicHTMLWebpage/BasicHTMLWebpage/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+document.addEventListener("DOMContentLoaded", function () {
+    var iframe = document.querySelector("iframe");
+
+    iframe.addEventListener("mouseover", function () {
+        iframe.style.transform = "scale(2)";
+    });
+
+    iframe.addEventListener("mouseout", function () {
+        iframe.style.transform = "scale(1)";
+    });
+});
---
+++ VS19BasicHTMLWebpage/JavaScript1.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var iframe = document.querySelector("iframe");
+
+    iframe.addEventListener("mouseover", function () {
+        iframe.style.transform = "scale(1.1)";
+    });
+
+    iframe.addEventListener("mouseout", function () {
+        iframe.style.transform = "scale(1)";
+    });
+});
